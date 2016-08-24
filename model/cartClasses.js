d81970d776e7700e36d54297ad39468070d2665b
Rae Roms <raeroms@RMBP.local>
2016-08-24 17:37:14 +0100

    Updated function for computing orders

--- model/cartClasses.js
+++ model/cartClasses.js
@@ -2,8 +2,23 @@ function basket (){
 	this.orderList = []; //stores all items dragged to basket
 	this.orderCount = 0;
 
-	this.addToBasket = function(){
-	
+	this.addToBasket = function (orderInst) {
+		this.orderList.push (orderInst);
+	}
+
+	this.computeOrder = function (this.orderList) {
+		
+		//var to hold total cost of purchase
+		var totalOrder = 0;
+		for (var i = 0; i < this.orderList.length; i++) {
+			//get each order in orderList
+			var eachOrder = this.orderList[i];
+
+			//compute purchase
+			totalOrder = totalOrder + (eachOrder.quantity * eachOrder.price);
+		}
+
+		return totalOrder;
 	}
 
 }
@@ -13,6 +28,7 @@ function product () {
 	this.picture = '';
 	this.price = 0;
 	this.discount = 0;
+	this.quantity = 0;  //set by view
 }
 
 function order () {
@@ -22,3 +38,21 @@ function order () {
 	this.price = 0;
 
 }
+
+function onDragged(productInst, basketInst){
+	
+	//create order for product
+	var orderInst = new order();
+	orderInst.productId = productInst.productId;
+	orderInst.quantity = productInst.quantity;
+	orderInst.price = productInst.price;
+
+	//add order to basket
+	basketInst.addToBasket(orderInst);
+
+}
+
+
+
+
+
